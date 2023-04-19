9ae3335ed3bc34455b7deddbb78c542f28b65be9
aim4dabush <edarasato@gmail.com>
2023-04-18 23:51:44 -0700

    edited shared link component

--- src/components/shared/link/Link.js
+++ src/components/shared/link/Link.js
@@ -3,12 +3,9 @@ import { NavLink } from "react-router-dom";
 //styles
 import styles from "./Link.module.scss";
 
-const Link = ({ classStyle, path, children }) => {
+const Link = ({ path, children }) => {
   return (
-    <NavLink
-      className={`${classStyle === "button" ? styles.btn : styles.link}`}
-      to={path}
-    >
+    <NavLink className={styles.link} to={path}>
       {children}
     </NavLink>
   );
