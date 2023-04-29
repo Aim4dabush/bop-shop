915ca834e6ba7bab3a2b7df1edc105ef07148718
aim4dabush <edarasato@gmail.com>
2023-04-29 16:58:33 -0700

    moved products fetch to products page

--- src/pages/products/ProductsPage.js
+++ src/pages/products/ProductsPage.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+
 //components
 import Products from "../../components/products/Products";
 
+//redux
+import { useDispatch, useSelector } from "react-redux";
+
+//thunks
+import { getProducts } from "../../redux/thunks/productsThunk";
+
 const ProductsPage = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products);
+
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(getProducts());
+    }
+  }, [dispatch, products]);
   return (
     <section>
       <Products />
