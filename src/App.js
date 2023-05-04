56be9c11ba7b17ec56a9dfbe4f66c9169321723d
aim4dabush <edarasato@gmail.com>
2023-05-03 18:44:15 -0700

    created an 3 sec timeout to close notification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Fragment, useEffect } from "react";
 
+//actions
+import { notificationActions } from "./redux/slices/notificationSlice";
+
 //components
 import Modal from "./modal/Modal";
 import NavBar from "./components/nav-bar/NavBar";
 import { Outlet } from "react-router-dom";
 
 //redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 //styles
 import styles from "./App.module.scss";
@@ -16,11 +19,25 @@ import { getShopData, getWishData } from "./redux/thunks/cartThunk";
 
 function App() {
   const dispatch = useDispatch();
+  const { show } = useSelector((state) => state.notification.info);
 
   useEffect(() => {
     dispatch(getShopData());
     dispatch(getWishData());
   }, [dispatch]);
+
+  useEffect(() => {
+    let closeNotification;
+    if (show) {
+      closeNotification = setTimeout(() => {
+        dispatch(notificationActions.setInfoReset());
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(closeNotification);
+    };
+  }, [dispatch, show]);
 
   return (
     <Fragment>
