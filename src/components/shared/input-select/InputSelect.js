96e07bfaa9ef8a9f6734ffc2538f67ce2bf74979
aim4dabush <edarasato@gmail.com>
2023-05-05 17:43:35 -0700

    added shared component input-select

---
+++ src/components/shared/input-select/InputSelect.js
@@ -0,0 +1,37 @@
+import Select from "react-select";
+
+//styles
+import styles from "./InputSelect.module.scss";
+
+const InputSelect = ({
+  children,
+  classStyle,
+  error,
+  id,
+  message,
+  onBlurHandler,
+  onChangeHandler,
+  selectOptions,
+  spaceLeft,
+  value,
+}) => {
+  return (
+    <div
+      className={`${styles.select_control} ${spaceLeft && styles.space_left}`}
+    >
+      <label htmlFor={id}>{children}</label>
+      <Select
+        classNamePrefix={styles.picker}
+        className={`${classStyle} ${styles.select}`}
+        id={id}
+        value={value}
+        onBlur={onBlurHandler}
+        onChange={onChangeHandler}
+        options={selectOptions}
+      />
+      {error && <p className={styles.error_message}>{message}</p>}
+    </div>
+  );
+};
+
+export default InputSelect;
