50eb380f2a93f008e73271bba9f856fb4cb5fda8
aim4dabush <edarasato@gmail.com>
2023-05-05 22:55:29 -0700

    fixed key error

--- src/components/checkout/checkout-list/CheckoutList.js
+++ src/components/checkout/checkout-list/CheckoutList.js
@@ -26,7 +26,7 @@ const CheckoutList = () => {
         <tbody>
           {shoppingCart.map((item) => {
             return (
-              <tr>
+              <tr key={item.id}>
                 <th>{item.title}</th>
                 <th>{item.quantity}</th>
                 <th>${item.price}</th>
